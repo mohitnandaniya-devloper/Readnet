2af6a8bc6523773f1b3595eb0c0e88911de536e2
Mohit Nandaniya <mohitnandaniya.mail@gmail.com>
2025-07-13 21:09:52 +0530

    setup routes and add plans page 13 june 2025

--- frontend/src/components/organisms/HeaderOrganism.jsx
+++ frontend/src/components/organisms/HeaderOrganism.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import SearchFormAtom from "@/components/atoms/SearchFormAtom"
+import ButtonAtom from "@/components/atoms/ButtonAtom"
 
-import { Navigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react"
 
 export default function HeaderOrganism() {
@@ -9,16 +10,18 @@ export default function HeaderOrganism() {
     <header class="bg-muted/50 body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <a class="flex title-font font-medium items-center mb-4 md:mb-0">
-            <span class="ml-3 text-xl">Readnet</span>
+            <span class="ml-3 text-4xl text-red-700">Readnet</span>
             </a>
             <div class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                <SearchFormAtom />
-                <SignedIn>
-                  <UserButton />
-                </SignedIn>
-                <SignedOut>
-                  <Navigate to="sign-in" replace/>
-                </SignedOut>
+            <SearchFormAtom />
+            <SignedOut>
+              <Link to="/sign-in">
+                <ButtonAtom children="Sign In" className="bg-red-600 text-white text-sm font-medium px-4 mx-2 rounded hover:bg-red-700" />
+              </Link>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
             </div>
         </div>
     </header>
